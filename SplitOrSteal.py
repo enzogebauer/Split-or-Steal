4ae654084fde1fe40a483f9070abfdf2221dbace
Enzo Gebauer <enzogebauer@MacBook-Air-de-Enzo.local>
2025-06-27 14:52:07 -0300

    add your agent with initial settings

--- SplitOrSteal.py
+++ SplitOrSteal.py
@@ -1,54 +1,13 @@
-import pygame
 import random
 import numpy as np
-from itertools import permutations, combinations
+from itertools import combinations
 import simple_opponents
 import your_agent
 import rl_agent
-
-# Initialize Pygame
-pygame.init()
-
-# Set up the screen
-screen_width = 1200
-screen_height = 400
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Split or Steal")
-
-# Define colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-
-# Load images
-split_img = pygame.image.load("split.png")
-steal_img = pygame.image.load("steal.png")
-doubt_img = pygame.image.load("card_back.png")
-background_image = pygame.image.load("background.png")
-background_image = pygame.transform.scale(
-    background_image, (screen_width, screen_height)
-)
-# Scale images
-image_scale = 0.5
-split_img = pygame.transform.scale(
-    split_img, (int(196 * image_scale), int(128 * image_scale))
-)
-steal_img = pygame.transform.scale(
-    steal_img, (int(196 * image_scale), int(128 * image_scale))
-)
-doubt_img = pygame.transform.scale(
-    doubt_img, (int(196 * image_scale), int(128 * image_scale))
-)
+import matplotlib.pyplot as plt
 
 mean = 100
 variance = 10000  # Large variance
-
-# Fonts
-font = pygame.font.SysFont(None, 24)
-
-# Game settings
-rounds_to_play = 10
 
 
 class Game:
@@ -61,13 +20,11 @@ class Game:
         return self.rounds_played >= self.total_rounds
 
     def prepare_round(self):
-        # Generate random values for total amount and rounds played
         self.current_amount = max(mean, np.random.normal(mean, np.sqrt(variance)))
 
     def play_round(self, left_agent, right_agent, remaining):
         self.rounds_played += 1
-
-        # Call the callback function with the generated values
+        self.prepare_round()
         left_decision = left_agent.decision(
             self.current_amount, remaining, left_agent.karma, right_agent.karma
         )
@@ -77,10 +34,6 @@ class Game:
         )
         assert right_decision in ["steal", "split"]
         decisions = np.array([left_decision, right_decision])
-        print(
-            f"Agent {left_agent.name}={left_decision}"
-            f" vs Agent {right_agent.name}={right_decision}"
-        )
 
         if all(decisions == "steal"):
             left_reward = 0
@@ -95,18 +48,12 @@ class Game:
             right_reward = self.current_amount
             left_reward = 0
 
-        print(
-            f"Agent {left_agent.name} won {left_reward:.2f}"
-            f" vs Agent {right_agent.name} won {right_reward:.2f}"
-        )
-
         left_agent.total_amount += left_reward
         right_agent.total_amount += right_reward
 
         left_agent.result(
             left_decision, right_decision, self.current_amount, left_reward
         )
-
         right_agent.result(
             right_decision, left_decision, self.current_amount, right_reward
         )
@@ -120,25 +67,6 @@ class Game:
             right_agent.add_karma(-1)
         else:
             right_agent.add_karma(+1)
-
-    def preround_render(self):
-        # Render agent's name and stats
-        rounds_text = font.render(
-            f"Rounds played: {self.rounds_played}/{self.total_rounds}", True, BLACK
-        )
-        screen.blit(rounds_text, (320, 50))
-
-        font_size = 72
-        myfont = pygame.font.Font(None, font_size)
-        amount_text = myfont.render(f"$ {self.current_amount:.2f}", True, BLACK)
-        screen.blit(amount_text, (350, 200))
-
-    def render(self):
-        # Render agent's name and stats
-        rounds_text = font.render(
-            f"Rounds played: {self.rounds_played}/{self.total_rounds}", True, BLACK
-        )
-        screen.blit(rounds_text, (320, 50))
 
 
 class Player:
@@ -155,53 +83,6 @@ class Player:
     def add_karma(self, value):
         self.karma = min(max(self.karma + value, -5), 5)
 
-    def render(self, x, y):
-        # Draw background rectangle
-        pygame.draw.rect(screen, BLACK, (x, y, 200, 80))
-
-        # Draw agent's name and stats
-        name_text = font.render(self.name, True, WHITE)
-        screen.blit(name_text, (x + 10, y + 10))
-        amount_text = font.render(f"Amount: {self.total_amount:.2f}", True, WHITE)
-        screen.blit(amount_text, (x + 10, y + 30))
-
-        if self.last_decision == "split":
-            screen.blit(split_img, (x + 150, y + 10))
-        elif self.last_decision == "steal":
-            screen.blit(steal_img, (x + 150, y + 10))
-
-    def preround_render(self, x, y):
-        # Draw agent's name and stats
-        pygame.draw.rect(screen, BLACK, (x, y, 200, 80))
-        karma_text = font.render(f"Karma: {self.karma}", True, WHITE)
-        screen.blit(karma_text, (x + 10, y + 50))
-
-        name_text = font.render(self.name, True, WHITE)
-        screen.blit(name_text, (x + 10, y + 10))
-
-        amount_text = font.render(f"Amount: {self.total_amount:.2f}", True, WHITE)
-        screen.blit(amount_text, (x + 10, y + 30))
-        screen.blit(doubt_img, (x + 150, y + 10))
-
-    def render(self, x, y):
-        # Draw background rectangle
-        pygame.draw.rect(screen, BLACK, (x, y, 200, 80))
-
-        karma_text = font.render(f"Karma: {self.karma}", True, WHITE)
-        screen.blit(karma_text, (x + 10, y + 50))
-
-        # Draw agent's name and stats
-        name_text = font.render(self.name, True, WHITE)
-        screen.blit(name_text, (x + 10, y + 10))
-        amount_text = font.render(f"Amount: {self.total_amount:.2f}", True, WHITE)
-        screen.blit(amount_text, (x + 10, y + 30))
-
-        # Draw decision image
-        if self.last_decision == "split":
-            screen.blit(split_img, (x + 150, y + 10))
-        elif self.last_decision == "steal":
-            screen.blit(steal_img, (x + 150, y + 10))
-
     def decision(self, total_amount, rounds_played, your_karma, his_karma):
         self.last_decision = self.agent.decision(
             total_amount, rounds_played, your_karma, his_karma
@@ -212,108 +93,116 @@ class Player:
         self.agent.result(your_action, his_action, total_possible, reward)
 
 
-def play_round(game, agent1, agent2, remaining):
-    print(f"{agent1.name} vs {agent2.name}")
-    # Clear the screen
-    screen.fill(BLACK)
-    screen.blit(background_image, (0, 0))
-    game.prepare_round()
-    game.preround_render()
-    agent1.preround_render(50, 50)
-    agent2.preround_render(550, 50)
+# --------- AUTOMATIZAÇÃO DE CENÁRIOS E ANÁLISE GRÁFICA ---------
 
-    # Update the screen
-    pygame.display.flip()
-    for _ in range(4):
-        pygame.time.wait(1)
+SCENARIOS = {
+    "Allgame": [
+        Player(simple_opponents.Splitter()),
+        Player(simple_opponents.Stealer()),
+        Player(simple_opponents.Randy()),
+        Player(simple_opponents.Karmine()),
+        Player(simple_opponents.Opportunist()),
+        Player(simple_opponents.Pretender()),
+        Player(your_agent.SmartRLAgent()),
+        Player(rl_agent.RLAgent()),
+    ],
+    "Simple": [
+        Player(simple_opponents.Karmine()),
+        Player(simple_opponents.Karmine()),
+        Player(rl_agent.RLAgent()),
+        Player(your_agent.SmartRLAgent()),
+    ],
+    "Difficult": [
+        Player(your_agent.SmartRLAgent()),
+        Player(your_agent.SmartRLAgent()),
+        Player(rl_agent.RLAgent()),
+        Player(your_agent.SmartRLAgent()),
+    ],
+    "Very difficult": [
+        Player(simple_opponents.Pretender()),
+        Player(simple_opponents.Pretender()),
+        Player(rl_agent.RLAgent()),
+        Player(simple_opponents.Karmine()),
+    ],
+    "Karma-aware": [
+        Player(simple_opponents.Karmine()),
+        Player(simple_opponents.Karmine()),
+        Player(rl_agent.RLAgent()),
+        Player(simple_opponents.Stealer()),
+    ],
+    "Opportunists": [
+        Player(simple_opponents.Opportunist()),
+        Player(simple_opponents.Opportunist()),
+        Player(rl_agent.RLAgent()),
+        Player(your_agent.SmartRLAgent()),
+    ],
+    "3 Karmines": [
+        Player(simple_opponents.Karmine()),
+        Player(simple_opponents.Karmine()),
+        Player(rl_agent.RLAgent()),
+        Player(simple_opponents.Karmine()),
+    ],
+    "RL vs YourAgent": [
+        Player(your_agent.SmartRLAgent()),
+        Player(rl_agent.RLAgent()),
+    ],
+}
 
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
 
-    # Play a round
-    game.play_round(agent1, agent2, remaining)
-
-    # Render agents
-    screen.fill(BLACK)
-    screen.blit(background_image, (0, 0))
-    agent1.render(50, 50)
-    agent2.render(550, 50)
-    game.render()
-
-    # Update the screen
-    pygame.display.flip()
-    for _ in range(4):
-        pygame.time.wait(100)
-        # pygame.time.wait(1)  # Para executar rápido
-
-
-# Create two agents
-agent1 = Player(simple_opponents.Splitter())
-agent2 = Player(simple_opponents.Stealer())
-agent3 = Player(simple_opponents.Randy())
-agent4 = Player(simple_opponents.Karmine())
-agent5 = Player(simple_opponents.Opportunist())
-agent6 = Player(simple_opponents.Pretender())
-agent_tittat = Player(your_agent.ReinforcementLearningAgent())
-
-# Allgame
-# agents = [agent1, agent2, agent3, agent4, agent5, agent6, ]
-
-# Simple
-agents = [
-    Player(simple_opponents.Karmine()),
-    Player(simple_opponents.Karmine()),
-    Player(rl_agent.RLAgent()),
-    agent_tittat,
-]
-
-# Difficult
-# agents = [Player(your_agent.ReinforcementLearningAgent()), Player(your_agent.ReinforcementLearningAgent()), Player(rl_agent.RLAgent()), Player(your_agent.ReinforcementLearningAgent())]
-
-# Very difficult
-# agents = [Player(simple_opponents.Pretender()), Player(simple_opponents.Pretender()), Player(rl_agent.RLAgent()), Player(simple_opponents.Karmine())]
-
-# Karma-aware
-# agents = [Player(simple_opponents.Karmine()), Player(simple_opponents.Karmine()), Player(rl_agent.RLAgent()), Player(simple_opponents.Stealer())]
-
-# Opportunists
-# agents = [Player(simple_opponents.Opportunist()),Player(simple_opponents.Opportunist()), Player(rl_agent.RLAgent()), agent_tittat]
-
-# 3 Karmines
-# agents = [Player(simple_opponents.Karmine()),  Player(simple_opponents.Karmine()), Player(rl_agent.RLAgent()), Player(simple_opponents.Karmine())]
-
-nrematches = 10  # Pode variar
-nfullrounds = 100  # Número de ciclos completos
-total_rounds = int(len(agents) * (len(agents) - 1) * nfullrounds * nrematches / 2)
-game = Game(total_rounds)
-
-from collections import defaultdict
-
-matches_played = defaultdict(lambda: 0)
-# Play rounds
-while not game.isOver():
-    random.shuffle(agents)
+def run_scenario(agents, nrematches=10, nfullrounds=100):
+    total_rounds = int(len(agents) * (len(agents) - 1) * nfullrounds * nrematches / 2)
+    game = Game(total_rounds)
     for a in agents:
-        a.reset_karma()
+        a.total_amount = 0
+        a.karma = 0
+    score_history = {a.name: [0] for a in agents}
+    while not game.isOver():
+        random.shuffle(agents)
+        for a in agents:
+            a.reset_karma()
+        for player1, player2 in combinations(agents, 2):
+            for remaining in reversed(range(0, nrematches)):
+                game.play_round(player1, player2, remaining)
+                for a in agents:
+                    score_history[a.name].append(a.total_amount)
+    return score_history
 
-    for player1, player2 in combinations(agents, 2):
-        matches_played[player1.name] += 1
-        matches_played[player2.name] += 1
-        print("==========")
-        for remaining in reversed(range(0, nrematches)):
-            play_round(game, player1, player2, remaining)
 
-print(matches_played)
-max_score = -1
-best = None
-scores = []
-for a in agents:
-    print(f"O agente '{a.name}' obteve {a.total_amount:.2f}")
-    if a.total_amount > max_score:
-        best = a
-        max_score = a.total_amount
-print(f"Vencedor: {best.name}")
-print(f"Score: {max_score:.2f}")
+def plot_results(all_histories):
+    for scenario, history in all_histories.items():
+        plt.figure(figsize=(10, 6))
+        for name, scores in history.items():
+            plt.plot(scores, label=name)
+        plt.title(f"Evolução da pontuação - {scenario}")
+        plt.xlabel("Iteração (rodada)")
+        plt.ylabel("Pontuação acumulada")
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+
+def main():
+    nrematches = 10
+    nfullrounds = 100
+    all_histories = {}
+
+    for scenario_name, agent_list in SCENARIOS.items():
+        agents = [
+            type(a.agent)() if hasattr(a, "agent") else type(a)() for a in agent_list
+        ]
+        agents = [Player(a) for a in agents]
+        print(f"\nRodando cenário: {scenario_name}")
+        history = run_scenario(agents, nrematches, nfullrounds)
+        all_histories[scenario_name] = history
+        final_scores = {name: scores[-1] for name, scores in history.items()}
+        sorted_results = sorted(final_scores.items(), key=lambda x: x[1], reverse=True)
+        print("Ranking final:")
+        for name, score in sorted_results:
+            print(f"  {name}: {score:.2f}")
+        print(f"Vencedor: {sorted_results[0][0]} com {sorted_results[0][1]:.2f}")
+
+    plot_results(all_histories)
+
+
+if __name__ == "__main__":
+    main()
--- your_agent.py
+++ your_agent.py
@@ -1,40 +1,115 @@
 import random
+from collections import defaultdict, deque
+import numpy as np
 
-class ReinforcementLearningAgent:
-  def __init__(self):
-  
-    # Lembrando a ultima acao
-    self.last_opponent_action = None
-    
-    # Flag indicando se essa seria a ultima rodada
-    self.last_round = False
-    
-  # Nome de seu agente deve ser colocado aqui  
-  def get_name(self):
-    return "Grupo/Apelido"
 
-  # Um exemplo basico de algo proximo de tit-for-tat
-  # apenas como demonstracao. Agente de aprendizagem
-  # por reforco seria o objetivo
-  def decision(self, amount, rounds_left, your_karma, his_karma):
-    print(f"{amount=}, {rounds_left=}, {your_karma=}, {his_karma=}")
-    self.last_round = True if rounds_left == 0 else False
-    
-    if self.last_opponent_action is None:
-      return "split"
-    elif self.last_opponent_action == "split":
-      return "split"
-    elif self.last_opponent_action == "steal":
-      return "steal"
-    else:
-      raise RuntimeError("Unknown action")
+class SmartRLAgent:
+    def __init__(self):
+        self.alpha = 0.5
+        self.gamma = 0.9
+        self.epsilon = 0.5  # começa explorando mais
+        self.epsilon_min = 0.05
+        self.epsilon_decay = 0.9995  # decai a cada decisão
+        self.Q = defaultdict(lambda: [0.0, 0.0])
+        self.action_list = ["split", "steal"]
+        self.last_opponent_actions = deque(
+            [0, 0], maxlen=2
+        )  # histórico das 2 últimas ações do oponente
+        self.last_your_actions = deque(
+            [0, 0], maxlen=2
+        )  # histórico das 2 últimas suas ações
+        self.last_round = False
+        self.current_input = None
+        self.current_output = None
+        self.mean_amount = 100  # valor médio do prêmio, para normalizar
 
-  # Receba as acoes de cada agente e o reward obtido (vs total possivel)
-  def result(self, your_action, his_action, total_possible, reward):
-    if self.last_round:
-      print("Forgetting last opponent action") # Vamos mudar de agente
-      self.last_opponent_action = None;
-    else:   
-      self.last_opponent_action = his_action;
-      print(f"For {self.get_name()=} {self.last_opponent_action=} ")    
+    def get_name(self):
+        return "SmartRL+"
 
+    def discretize_amount(self, amount):
+        # Discretiza o valor do prêmio em 3 faixas: baixo, médio, alto
+        if amount < 80:
+            return 0
+        elif amount < 120:
+            return 1
+        else:
+            return 2
+
+    def extract_rl_state(self, state):
+        # Estado: (rodadas_restantes, seu_karma, karma_oponente, últimas ações, faixa_prêmio)
+        return (
+            state[1],  # rounds_left
+            np.sign(state[2]),  # seu karma
+            np.sign(state[3]),  # karma do oponente
+            tuple(state[4]),  # últimas ações do oponente
+            tuple(state[5]),  # suas últimas ações
+            state[6],  # faixa do prêmio
+        )
+
+    def choose_action(self, state):
+        state = self.extract_rl_state(state)
+        if np.random.uniform(0, 1) < self.epsilon:
+            return np.random.choice(self.action_list)
+        else:
+            return self.action_list[np.argmax(self.Q[state])]
+
+    def update_qtable(self, state, action, reward, next_state):
+        alp = self.alpha
+        gam = self.gamma
+        action_index = self.action_list.index(action)
+        state = self.extract_rl_state(state)
+        next_state = self.extract_rl_state(next_state)
+        self.Q[state][action_index] = (1 - alp) * self.Q[state][action_index] + alp * (
+            reward + gam * np.max(self.Q[next_state])
+        )
+
+    def decision(self, amount, rounds_left, your_karma, his_karma):
+        # Epsilon decrescente
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+        amount_bin = self.discretize_amount(amount)
+        # Estado inclui histórico das últimas 2 ações do oponente e suas
+        novel_input = (
+            amount,
+            rounds_left,
+            your_karma,
+            his_karma,
+            list(self.last_opponent_actions),
+            list(self.last_your_actions),
+            amount_bin,
+        )
+        if self.current_input is not None:
+            self.update_qtable(
+                self.current_input,
+                self.current_output[0],
+                self.current_output[-1],
+                novel_input,
+            )
+        self.current_input = novel_input
+        action = self.choose_action(self.current_input)
+        # Atualiza histórico
+        self.last_your_actions.append(1 if action == "steal" else -1)
+        return action
+
+    def result(self, your_action, his_action, total_possible, reward):
+        # Atualiza histórico do oponente
+        self.last_opponent_actions.append(1 if his_action == "steal" else -1)
+        # Penalidade para karma negativo
+        karma_penalty = -0.2 if self.current_input[2] < 0 else 0
+        # Recompensa baseada no valor real ganho, normalizada
+        if your_action == "steal" and his_action == "steal":
+            reward = 0 + karma_penalty
+        elif your_action == "steal" and his_action == "split":
+            reward = (total_possible / self.mean_amount) + karma_penalty
+        elif your_action == "split" and his_action == "split":
+            reward = (
+                (total_possible / self.mean_amount) * 0.6 + 0.2 + karma_penalty
+            )  # bônus para cooperação
+        elif your_action == "split" and his_action == "steal":
+            reward = -0.5 + karma_penalty
+        self.current_output = (your_action, his_action, total_possible, reward)
+        # Zera histórico se for última rodada
+        if self.current_input and self.current_input[1] == 0:
+            self.last_opponent_actions = deque([0, 0], maxlen=2)
+            self.last_your_actions = deque([0, 0], maxlen=2)
